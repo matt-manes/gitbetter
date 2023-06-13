4a4b6fb83685746bde70d5b5a4e3d02f1f09295c
matt manes <mattmanes@pm.me>
2023-06-13 17:26:54 -0500

    feat: add all core git commands to gitbetter shell

--- src/gitbetter/gitbetter.py
+++ src/gitbetter/gitbetter.py
@@ -51,49 +51,303 @@ class GitBetter(ArgShell):
         os.chdir(path)
         self.prompt = f"gitbetter::{Pathier.cwd()}>"
 
-    def do_git(self, arg: str):
-        """Directly execute `git {arg}`.
+    # Seat |================================================Core================================================|
+    def do_git(self, args: str):
+        """Directly execute `git {args}`.
 
         i.e. You can still do everything directly invoking git can do."""
-        self.git.git(arg)
+        self.git.git(args)
 
-    def do_add(self, args: Namespace):
-        """Stage a list of files.
-        If no files are given, all files will be added."""
-        if not args.files:
-            self.git.add_all()
-        else:
-            self.git.add_files(args.files)
+    # Seat
+    def do_add(self, args: str):
+        """>>> git add {args}"""
+        self.git.add(args)
+
+    def do_am(self, args: str):
+        """>>> git am {args}"""
+        self.git.am(args)
+
+    def do_annotate(self, args: str):
+        """>>> git annotate {args}"""
+        self.git.annotate(args)
+
+    def do_archive(self, args: str):
+        """>>> git archive {args}"""
+        self.git.archive(args)
+
+    def do_bisect(self, args: str):
+        """>>> git bisect {args}"""
+        self.git.bisect(args)
+
+    def do_blame(self, args: str):
+        """>>> git blame {args}"""
+        self.git.blame(args)
+
+    def do_branch(self, args: str):
+        """>>> git branch {args}"""
+        self.git.branch(args)
+
+    def do_bugreport(self, args: str):
+        """>>> git bugreport {args}"""
+        self.git.bugreport(args)
+
+    def do_bundle(self, args: str):
+        """>>> git bundle {args}"""
+        self.git.bundle(args)
+
+    def do_checkout(self, args: str):
+        """>>> git checkout {args}"""
+        self.git.checkout(args)
+
+    def do_cherry_pick(self, args: str):
+        """>>> git cherry_pick {args}"""
+        self.git.cherry_pick(args)
+
+    def do_citool(self, args: str):
+        """>>> git citool {args}"""
+        self.git.citool(args)
+
+    def do_clean(self, args: str):
+        """>>> git clean {args}"""
+        self.git.clean(args)
+
+    def do_clone(self, args: str):
+        """>>> git clone {args}"""
+        self.git.clone(args)
 
     def do_commit(self, args: str):
-        """Commit staged files with provided `args` string."""
+        """>>> git commit {args}"""
         self.git.commit(args)
 
-    def do_push(self, args: str):
-        """Execute `git push`.
+    def do_config(self, args: str):
+        """>>> git config {args}"""
+        self.git.config(args)
 
-        `args` can be any additional args that `git push` accepts."""
-        self.git.push(args)
+    def do_count_objects(self, args: str):
+        """>>> git count_objects {args}"""
+        self.git.count_objects(args)
 
-    def do_pull(self, args: str):
-        """Execute `git pull`.
+    def do_describe(self, args: str):
+        """>>> git describe {args}"""
+        self.git.describe(args)
 
-        `args` can be any additional args that `git pull` accepts."""
-        self.git.pull(args)
+    def do_diagnose(self, args: str):
+        """>>> git diagnose {args}"""
+        self.git.diagnose(args)
 
-    def do_status(self, _: str):
-        """Execute `git status`."""
-        self.git.status()
+    def do_diff(self, args: str):
+        """>>> git diff {args}"""
+        self.git.diff(args)
+
+    def do_difftool(self, args: str):
+        """>>> git difftool {args}"""
+        self.git.difftool(args)
+
+    def do_fast_export(self, args: str):
+        """>>> git fast_export {args}"""
+        self.git.fast_export(args)
+
+    def do_fast_import(self, args: str):
+        """>>> git fast_import {args}"""
+        self.git.fast_import(args)
+
+    def do_fetch(self, args: str):
+        """>>> git fetch {args}"""
+        self.git.fetch(args)
+
+    def do_filter_branch(self, args: str):
+        """>>> git filter_branch {args}"""
+        self.git.filter_branch(args)
+
+    def do_format_patch(self, args: str):
+        """>>> git format_patch {args}"""
+        self.git.format_patch(args)
+
+    def do_fsck(self, args: str):
+        """>>> git fsck {args}"""
+        self.git.fsck(args)
+
+    def do_gc(self, args: str):
+        """>>> git gc {args}"""
+        self.git.gc(args)
+
+    def do_gitk(self, args: str):
+        """>>> git gitk {args}"""
+        self.git.gitk(args)
+
+    def do_gitweb(self, args: str):
+        """>>> git gitweb {args}"""
+        self.git.gitweb(args)
+
+    def do_grep(self, args: str):
+        """>>> git grep {args}"""
+        self.git.grep(args)
+
+    def do_gui(self, args: str):
+        """>>> git gui {args}"""
+        self.git.gui(args)
+
+    def do_init(self, args: str):
+        """>>> git init {args}"""
+        self.git.init(args)
+
+    def do_instaweb(self, args: str):
+        """>>> git instaweb {args}"""
+        self.git.instaweb(args)
+
+    def do_log(self, args: str):
+        """>>> git log {args}"""
+        self.git.log(args)
+
+    def do_maintenance(self, args: str):
+        """>>> git maintenance {args}"""
+        self.git.maintenance(args)
 
     def do_merge(self, args: str):
-        """Perform merge operation with supplied `args`."""
+        """>>> git merge {args}"""
         self.git.merge(args)
 
+    def do_merge_tree(self, args: str):
+        """>>> git merge_tree {args}"""
+        self.git.merge_tree(args)
+
+    def do_mergetool(self, args: str):
+        """>>> git mergetool {args}"""
+        self.git.mergetool(args)
+
+    def do_mv(self, args: str):
+        """>>> git mv {args}"""
+        self.git.mv(args)
+
+    def do_notes(self, args: str):
+        """>>> git notes {args}"""
+        self.git.notes(args)
+
+    def do_pack_refs(self, args: str):
+        """>>> git pack_refs {args}"""
+        self.git.pack_refs(args)
+
+    def do_prune(self, args: str):
+        """>>> git prune {args}"""
+        self.git.prune(args)
+
+    def do_pull(self, args: str):
+        """>>> git pull {args}"""
+        self.git.pull(args)
+
+    def do_push(self, args: str):
+        """>>> git push {args}"""
+        self.git.push(args)
+
+    def do_range_diff(self, args: str):
+        """>>> git range_diff {args}"""
+        self.git.range_diff(args)
+
+    def do_rebase(self, args: str):
+        """>>> git rebase {args}"""
+        self.git.rebase(args)
+
+    def do_reflog(self, args: str):
+        """>>> git reflog {args}"""
+        self.git.reflog(args)
+
+    def do_remote(self, args: str):
+        """>>> git remote {args}"""
+        self.git.remote(args)
+
+    def do_repack(self, args: str):
+        """>>> git repack {args}"""
+        self.git.repack(args)
+
+    def do_replace(self, args: str):
+        """>>> git replace {args}"""
+        self.git.replace(args)
+
+    def do_request_pull(self, args: str):
+        """>>> git request_pull {args}"""
+        self.git.request_pull(args)
+
+    def do_rerere(self, args: str):
+        """>>> git rerere {args}"""
+        self.git.rerere(args)
+
+    def do_reset(self, args: str):
+        """>>> git reset {args}"""
+        self.git.reset(args)
+
+    def do_restore(self, args: str):
+        """>>> git restore {args}"""
+        self.git.restore(args)
+
+    def do_revert(self, args: str):
+        """>>> git revert {args}"""
+        self.git.revert(args)
+
+    def do_rm(self, args: str):
+        """>>> git rm {args}"""
+        self.git.rm(args)
+
+    def do_scalar(self, args: str):
+        """>>> git scalar {args}"""
+        self.git.scalar(args)
+
+    def do_shortlog(self, args: str):
+        """>>> git shortlog {args}"""
+        self.git.shortlog(args)
+
+    def do_show(self, args: str):
+        """>>> git show {args}"""
+        self.git.show(args)
+
+    def do_show_branch(self, args: str):
+        """>>> git show_branch {args}"""
+        self.git.show_branch(args)
+
+    def do_sparse_checkout(self, args: str):
+        """>>> git sparse_checkout {args}"""
+        self.git.sparse_checkout(args)
+
+    def do_stash(self, args: str):
+        """>>> git stash {args}"""
+        self.git.stash(args)
+
+    def do_status(self, args: str):
+        """>>> git status {args}"""
+        self.git.status(args)
+
+    def do_submodule(self, args: str):
+        """>>> git submodule {args}"""
+        self.git.submodule(args)
+
+    def do_switch(self, args: str):
+        """>>> git switch {args}"""
+        self.git.switch(args)
+
     def do_tag(self, args: str):
-        """Tag current commit with `tag_id`."""
+        """>>> git tag {args}"""
         self.git.tag(args)
 
-    # |==================================Convenience==================================|
+    def do_verify_commit(self, args: str):
+        """>>> git verify_commit {args}"""
+        self.git.verify_commit(args)
+
+    def do_verify_tag(self, args: str):
+        """>>> git verify_tag {args}"""
+        self.git.verify_tag(args)
+
+    def do_version(self, args: str):
+        """>>> git version {args}"""
+        self.git.version(args)
+
+    def do_whatchanged(self, args: str):
+        """>>> git whatchanged {args}"""
+        self.git.whatchanged(args)
+
+    def do_worktree(self, args: str):
+        """>>> git worktree {args}"""
+        self.git.worktree(args)
+
+    # Seat |==================================Convenience==================================|
 
     def do_new_repo(self, _: str):
         """Create a new git repo in this directory."""
@@ -131,10 +385,6 @@ class GitBetter(ArgShell):
             message += '"'
         self.git.add_all()
         self.git.commit(f"-m {message}")
-
-    def do_switch(self, branch_name: str):
-        """Switch to this branch."""
-        self.git.switch_branch(branch_name)
 
     def do_add_url(self, url: str):
         """Add remote origin url for repo and push repo."""
