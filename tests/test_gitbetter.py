3c0433ccf1b06a0787578b03309ffac23a423c66
matt manes <mattmanes@pm.me>
2023-06-13 19:42:22 -0500

    tests: add test

--- tests/test_gitbetter.py
+++ tests/test_gitbetter.py
@@ -1,3 +1,39 @@
 import pytest
 
-from gitbetter import gitbetter
+from gitbetter import Git
+from pathier import Pathier
+import os
+
+root = Pathier(__file__).parent
+
+
+def test_basics():
+    proj = root / "proj"
+    proj.delete()
+    proj.mkdir()
+    proj.mkcwd()
+    git = Git()
+    git.new_repo()
+    (proj / "file.py").write_text("file = 'test'")
+    git.initcommit([proj / "file.py"])
+    assert git.current_branch == "main"
+    git.create_new_branch("new-test")
+    assert git.current_branch == "new-test"
+    (proj / "file.py").write_text("file = 'test2'")
+    git.commit_files([proj / "file.py"], "refactor: change string value")
+    (proj / "file2.py").write_text("import gitbetter")
+    git.initcommit([proj / "file2.py"])
+    (proj / "stuff.txt").write_text("stuff")
+    git.ignore(["stuff.txt"])
+    git.commit_all("chore: add to gitignore")
+    git.ignore(["*.txt"])
+    git.amend()
+    git.switch_branch("main")
+    git.merge("new-test")
+    with git.capture_output():
+        assert len(git.list_branches().splitlines()) == 2  # type: ignore
+    git.delete_branch("new-test")
+    with git.capture_output():
+        assert len(git.list_branches().splitlines()) == 1  # type: ignore
+    git.loggy()
+    git.status()
